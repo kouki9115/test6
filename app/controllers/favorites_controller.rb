36f65fee874bb99b36e06caf9e3d8e633d622ef0
kouki9115 <kouki.n9115@gmail.com>
2021-04-10 06:41:44 +0000

    Update

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @book = Book.find(params[:book_id])
+    @book = Book.find(params[:book])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
   end
